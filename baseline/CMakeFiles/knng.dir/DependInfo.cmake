b65c126f5d4902f15fa5a3974c78ea403117ce15
mashiroyuki <69560585+mashiroyuki02@users.noreply.github.com>
2023-03-02 18:37:31 +0800

    Add files via upload

---
+++ baseline/CMakeFiles/knng.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/h/baoyan/SIGMOD/src/baseline/HyRec.cpp" "CMakeFiles/knng.dir/HyRec.cpp.o" "gcc" "CMakeFiles/knng.dir/HyRec.cpp.o.d"
+  "/mnt/h/baoyan/SIGMOD/src/baseline/kdtree.cc" "CMakeFiles/knng.dir/kdtree.cc.o" "gcc" "CMakeFiles/knng.dir/kdtree.cc.o.d"
+  "/mnt/h/baoyan/SIGMOD/src/baseline/knn-construction.cc" "CMakeFiles/knng.dir/knn-construction.cc.o" "gcc" "CMakeFiles/knng.dir/knn-construction.cc.o.d"
+  "/mnt/h/baoyan/SIGMOD/src/baseline/utils.cpp" "CMakeFiles/knng.dir/utils.cpp.o" "gcc" "CMakeFiles/knng.dir/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
