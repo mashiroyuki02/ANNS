f17da1002bd6471f98187ecb9b49bcce63709a1f
mashiroyuki <69560585+mashiroyuki02@users.noreply.github.com>
2023-03-02 20:55:33 +0800

    Update config.h

--- config.h
+++ config.h
@@ -2,18 +2,22 @@
 #define __CONFIG_H__
 
 #define MAX_RUN_SECONDS  30 * 60 
-#define NNodes 10000
+extern int NNodes;
+// #define NNodes 1000000
 
 
 /// parameters for HyRec
 #define MAX_EPOCH 10
-#define MIN_CHANGES 1000
+#define MIN_CHANGES 150000
 
 /// LOG
-#define LOGGAP 10000
+#define LOGGAP 1000000
 
 /// Early Stop
-#define UpdThresh 0.01
-#define STOPTIMES 2
+#define UpdThresh 0.00001
+#define STOPTIMES 4
+
+/// OnLine Test
+#define ONLINE_JUDGE
 
 #endif
