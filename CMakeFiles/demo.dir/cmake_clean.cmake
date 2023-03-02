ac43ce075cd268554c4fb87109ed367cf6e20ca9
mashiroyuki <69560585+mashiroyuki02@users.noreply.github.com>
2023-03-02 18:44:37 +0800

    Add files via upload

---
+++ CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/kdtree.cc.o"
+  "CMakeFiles/demo.dir/kdtree.cc.o.d"
+  "CMakeFiles/demo.dir/knn-construction.cc.o"
+  "CMakeFiles/demo.dir/knn-construction.cc.o.d"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/knng.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/knng.dir/HyRec.cpp.o"
+  "CMakeFiles/knng.dir/HyRec.cpp.o.d"
+  "CMakeFiles/knng.dir/kdtree.cc.o"
+  "CMakeFiles/knng.dir/kdtree.cc.o.d"
+  "CMakeFiles/knng.dir/knn-construction.cc.o"
+  "CMakeFiles/knng.dir/knn-construction.cc.o.d"
+  "CMakeFiles/knng.dir/utils.cpp.o"
+  "CMakeFiles/knng.dir/utils.cpp.o.d"
+  "knng"
+  "knng.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/knng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
